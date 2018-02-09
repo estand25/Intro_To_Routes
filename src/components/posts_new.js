09558e74eaec7200e6ba4d82ea3780976b9efcb5
Standley Eugene <estand25@gmail.com>
2018-02-08 19:01:20 -0500

    Lecture 133 - Setting up Redux Form

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class PostNew extends Component{
   render() {
     return (
-      <div>
-        PostsNew!
-      </div>
+      <form>
+        <Field
+          name="title"
+          component={}
+        />
+      </form>
     );
   }
 }
 
-export default PostNew;
+export default reduxForm({
+  form: 'PostsNewForm'
+}) (PostNew);

c15f583ae65affe397d77ff83790ecf54c5bb315
Standley Eugene <estand25@gmail.com>
2018-02-21 19:32:01 -0500

    Lecture 142 - Create Post action Creator

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
 
 class PostNew extends Component{
   renderField(field){
@@ -44,11 +45,8 @@ class PostNew extends Component{
           name="content"
           component={this.renderField}
         />
-        <button
-          type="submit"
-          className="btn btn-primary">
-            Submit
-          </button>
+        <button type="submit" className="btn btn-primary">Submit</button>
+        <Link to="/" className="btn btn-danger">Cancel</Link>
       </form>
     );
   }
